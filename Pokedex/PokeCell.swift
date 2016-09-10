25dcd43d2214a5ab1944a73248229228b9fba019
DevXris <chuwei0915@gmail.com>
2016-09-10 17:40:43 +0800

    2nd commit
    Add PokeCell and Pokemon class

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Chris Huang on 9/10/16.
+//  Copyright © 2016 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,23 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Chris Huang on 9/10/16.
+//  Copyright © 2016 Chris Huang. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String { return _name ?? "" }
+    var pokedexId: Int { return _pokedexId ?? 0 }
+    
+    init(name: String, pokedexId: Int) {
+        _name = name
+        _pokedexId = pokedexId
+    }
+}
